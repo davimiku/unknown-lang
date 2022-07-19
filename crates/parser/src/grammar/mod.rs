2b5739af42cade1ca5561bfd78aed8555bf65fd5
David Mikulis <davidmikulis@gmail.com>
2022-07-19 07:25:57 -0500

    add parse entrypoint for Expr

--- crates/parser/src/grammar/mod.rs
+++ crates/parser/src/grammar/mod.rs
@@ -2,15 +2,22 @@ mod expr;
 mod stmt;
 
 use crate::parser::marker::CompletedMarker;
-use crate::parser::Parser;
+use crate::parser::{ParseEntryPoint, Parser};
 use crate::syntax::SyntaxKind;
 
 pub(crate) fn root(p: &mut Parser) -> CompletedMarker {
     let m = p.start();
 
-    while !p.at_end() {
-        stmt::parse_stmt(p);
-    }
+    match p.entry_point {
+        ParseEntryPoint::Root => {
+            while !p.at_end() {
+                stmt::parse_stmt(p);
+            }
+        }
+        ParseEntryPoint::Expr => {
+            expr::parse_expr(p);
+        }
+    };
 
     m.complete(p, SyntaxKind::Root)
 }
