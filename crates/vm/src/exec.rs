d6bfa8ebd04341eb404d8c0d580cc7b0bf961205
David Mikulis <davidmikulis@gmail.com>
2022-07-19 07:27:00 -0500

    moves chunk to separate crate

--- crates/vm/src/exec.rs
+++ crates/vm/src/exec.rs
@@ -1,5 +1,4 @@
-use crate::chunk::{Chunk, Op};
-use crate::value::ValueStack;
+use chunk::{Chunk, Op, ValueStack};
 
 pub(crate) struct VM<'a> {
     /// Chunk of bytecode to run
@@ -25,58 +24,83 @@ impl<'a> VM<'a> {
         self.run()
     }
 
+    #[cfg(test)]
+    fn debug_print(&self, op: &Op) {
+        {
+            print!("     ");
+            for i in 0..self.stack.current {
+                print!("[ ");
+                print!("{:?}", self.stack.peek_at(i));
+                print!(" ]")
+            }
+            println!();
+            println!("{}", op.disassemble(self.chunk));
+        }
+    }
+
     fn run(&mut self) -> InterpretResult {
         let result = loop {
             let op = self.chunk.get_op(self.ip);
 
-            // debug prints
-            #[cfg(test)]
-            {
-                print!("     ");
-                for i in 0..self.stack.current {
-                    print!("[ ");
-                    print!("{}", self.stack.values[i]);
-                    print!(" ]")
-                }
-                print!("\n");
-                println!("{}", op.disassemble(self.chunk));
-            }
+            // #[cfg(test)]
+            // self.debug_print(op);
 
             match op {
                 Op::Constant(i) => {
-                    let constant = self.chunk.get_constant(*i);
-                    self.stack.push(constant);
+                    let constant = self.chunk.get_int(*i);
+                    self.stack.push(constant.to_le_bytes());
                 }
-                // TODO: possibly use macros
+                Op::FConstant(i) => {
+                    let constant = self.chunk.get_float(*i);
+                    println!("FConstant({}, {})", *i, constant);
+                    self.stack.push(constant.to_le_bytes());
+                }
+                // TODO: investigate macros to reduce boilerplate
                 Op::FAdd => {
-                    let b = self.stack.pop();
-                    let a = self.stack.pop();
-                    self.stack.push(a + b);
+                    let b: [u8; 8] = self.stack.pop();
+                    let b = f64::from_le_bytes(b);
+
+                    let a: [u8; 8] = self.stack.pop();
+                    let a = f64::from_le_bytes(a);
+
+                    self.stack.push((a + b).to_le_bytes());
                 }
-                Op::FSubtract => {
-                    let b = self.stack.pop();
-                    let a = self.stack.pop();
-                    self.stack.push(a - b);
+                Op::FSub => {
+                    let b: [u8; 8] = self.stack.pop();
+                    let b = f64::from_le_bytes(b);
+
+                    let a: [u8; 8] = self.stack.pop();
+                    let a = f64::from_le_bytes(a);
+
+                    self.stack.push((a - b).to_le_bytes());
                 }
-                Op::FMultiply => {
-                    let b = self.stack.pop();
-                    let a = self.stack.pop();
-                    self.stack.push(a * b);
+                Op::FMul => {
+                    let b: [u8; 8] = self.stack.pop();
+                    let b = f64::from_le_bytes(b);
+
+                    let a: [u8; 8] = self.stack.pop();
+                    let a = f64::from_le_bytes(a);
+
+                    self.stack.push((a * b).to_le_bytes());
                 }
-                Op::FDivide => {
-                    let b = self.stack.pop();
-                    let a = self.stack.pop();
-                    self.stack.push(a / b);
+                Op::FDiv => {
+                    let b: [u8; 8] = self.stack.pop();
+                    let b = f64::from_le_bytes(b);
+
+                    let a: [u8; 8] = self.stack.pop();
+                    let a = f64::from_le_bytes(a);
+
+                    // TODO: runtime error for 0 denominator
+
+                    self.stack.push((a / b).to_le_bytes());
                 }
-                Op::Negate => {
-                    let value = self.stack.pop();
-                    self.stack.push(-value);
-                }
-                Op::Return => {
-                    println!("{}", self.stack.pop());
+                Op::Ret => {
+                    // TODO: better output for debugging?
+                    // dbg!(self.stack.pop::<[u8; 8]>());
 
                     break InterpretResult::Ok;
                 }
+                _ => unimplemented!(),
             }
 
             self.ip += 1;
@@ -84,6 +108,18 @@ impl<'a> VM<'a> {
 
         result
     }
+}
+
+// TODO: how to use this inside a function
+#[macro_export]
+macro_rules! pop_floats {
+    () => {
+        let b: [u8; 8] = self.stack.pop();
+        let b = f64::from_le_bytes(b);
+
+        let a: [u8; 8] = self.stack.pop();
+        let a = f64::from_le_bytes(a);
+    };
 }
 
 pub(crate) enum InterpretResult {
--- crates/vm/src/lib.rs
+++ crates/vm/src/lib.rs
@@ -1,29 +1,25 @@
 use chunk::{Chunk, Op};
 use exec::VM;
 
-mod chunk;
 mod exec;
-mod value;
 
 pub fn run() {
     let mut chunk = Chunk::new();
 
-    chunk.write_constant(1.2, 123);
-    chunk.write_constant(3.4, 123);
+    chunk.write_float_constant(1.2, 123);
+    chunk.write_float_constant(3.4, 123);
     chunk.write(Op::FAdd, 123);
 
-    chunk.write_constant(5.6, 123);
-    chunk.write(Op::FDivide, 123);
+    chunk.write_float_constant(5.6, 123);
+    chunk.write(Op::FDiv, 123);
 
-    chunk.write_constant(1.0, 123);
-    chunk.write(Op::FSubtract, 123);
+    chunk.write_float_constant(1.0, 123);
+    chunk.write(Op::FSub, 123);
 
-    chunk.write_constant(20.0, 123);
-    chunk.write(Op::FMultiply, 123);
+    chunk.write_float_constant(20.0, 123);
+    chunk.write(Op::FMul, 123);
 
-    chunk.write(Op::Negate, 123);
-
-    chunk.write(Op::Return, 123);
+    chunk.write(Op::Ret, 123);
 
     chunk.disassemble("test chunk");
 
