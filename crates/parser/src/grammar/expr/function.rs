7d4ca41d2ecd452891156ad7ba338604381950f6
David Mikulis <davidmikulis@gmail.com>
2022-07-19 07:25:42 -0500

    rename parse nodes in tests

--- crates/parser/src/grammar/expr/function.rs
+++ crates/parser/src/grammar/expr/function.rs
@@ -80,41 +80,34 @@ fn parse_fun_body(p: &mut Parser) -> CompletedMarker {
 
 #[cfg(test)]
 mod tests {
-    use crate::check;
+    use crate::check_expr;
     use expect_test::expect;
 
     #[test]
     fn parse_empty_function() {
-        check(
-            "let x = fun () -> {}",
+        check_expr(
+            "fun () -> {}",
             expect![[r#"
-Root@0..20
-  VariableDef@0..20
-    Let@0..3 "let"
+Root@0..12
+  FunExpr@0..12
+    Fun@0..3 "fun"
     Emptyspace@3..4 " "
-    Ident@4..5 "x"
-    Emptyspace@5..6 " "
-    Equals@6..7 "="
-    Emptyspace@7..8 " "
-    FunExpr@8..20
-      Fun@8..11 "fun"
-      Emptyspace@11..12 " "
-      LParen@12..13 "("
-      FunParamList@13..13
-      RParen@13..14 ")"
-      Emptyspace@14..15 " "
-      Arrow@15..17 "->"
-      Emptyspace@17..18 " "
-      FunBody@18..20
-        BlockExpr@18..20
-          LBrace@18..19 "{"
-          RBrace@19..20 "}""#]],
+    LParen@4..5 "("
+    FunParamList@5..5
+    RParen@5..6 ")"
+    Emptyspace@6..7 " "
+    Arrow@7..9 "->"
+    Emptyspace@9..10 " "
+    FunBody@10..12
+      BlockExpr@10..12
+        LBrace@10..11 "{"
+        RBrace@11..12 "}""#]],
         );
     }
 
     #[test]
     fn parse_function_block_body() {
-        check(
+        check_expr(
             r#"fun () -> {
           let a = 1
           let b = 2
@@ -144,7 +137,7 @@ Root@0..77
           Equals@28..29 "="
           Emptyspace@29..30 " "
           IntExpr@30..31
-            Int@30..31 "1"
+            IntLiteral@30..31 "1"
         Newline@31..32 "\n"
         Emptyspace@32..42 "          "
         VariableDef@42..51
@@ -155,16 +148,16 @@ Root@0..77
           Equals@48..49 "="
           Emptyspace@49..50 " "
           IntExpr@50..51
-            Int@50..51 "2"
+            IntLiteral@50..51 "2"
         Newline@51..52 "\n"
         Emptyspace@52..62 "          "
         InfixExpr@62..67
-          IdentExpr@62..64
+          NameRef@62..64
             Ident@62..63 "a"
             Emptyspace@63..64 " "
           Plus@64..65 "+"
           Emptyspace@65..66 " "
-          IdentExpr@66..67
+          NameRef@66..67
             Ident@66..67 "b"
         Newline@67..68 "\n"
         Emptyspace@68..76 "        "
@@ -174,7 +167,7 @@ Root@0..77
 
     #[test]
     fn parse_function_return_type() {
-        check(
+        check_expr(
             "fun () -> int { 1 }",
             expect![[r#"
 Root@0..19
@@ -188,7 +181,7 @@ Root@0..19
     Arrow@7..9 "->"
     Emptyspace@9..10 " "
     TypeExpr@10..14
-      IdentExpr@10..14
+      NameRef@10..14
         Ident@10..13 "int"
         Emptyspace@13..14 " "
     FunBody@14..19
@@ -196,7 +189,7 @@ Root@0..19
         LBrace@14..15 "{"
         Emptyspace@15..16 " "
         IntExpr@16..18
-          Int@16..17 "1"
+          IntLiteral@16..17 "1"
           Emptyspace@17..18 " "
         RBrace@18..19 "}""#]],
         )
@@ -204,7 +197,7 @@ Root@0..19
 
     #[test]
     fn parse_function_one_param() {
-        check(
+        check_expr(
             "fun (a: int) -> int { a + 1 }",
             expect![[r#"
 Root@0..29
@@ -218,14 +211,14 @@ Root@0..29
         Colon@6..7 ":"
         Emptyspace@7..8 " "
         TypeExpr@8..11
-          IdentExpr@8..11
+          NameRef@8..11
             Ident@8..11 "int"
     RParen@11..12 ")"
     Emptyspace@12..13 " "
     Arrow@13..15 "->"
     Emptyspace@15..16 " "
     TypeExpr@16..20
-      IdentExpr@16..20
+      NameRef@16..20
         Ident@16..19 "int"
         Emptyspace@19..20 " "
     FunBody@20..29
@@ -233,13 +226,13 @@ Root@0..29
         LBrace@20..21 "{"
         Emptyspace@21..22 " "
         InfixExpr@22..28
-          IdentExpr@22..24
+          NameRef@22..24
             Ident@22..23 "a"
             Emptyspace@23..24 " "
           Plus@24..25 "+"
           Emptyspace@25..26 " "
           IntExpr@26..28
-            Int@26..27 "1"
+            IntLiteral@26..27 "1"
             Emptyspace@27..28 " "
         RBrace@28..29 "}""#]],
         )
@@ -247,7 +240,7 @@ Root@0..29
 
     #[test]
     fn parse_function_two_params() {
-        check(
+        check_expr(
             "fun (a: int, b: int) -> int { a + b }",
             expect![[r#"
 Root@0..37
@@ -261,7 +254,7 @@ Root@0..37
         Colon@6..7 ":"
         Emptyspace@7..8 " "
         TypeExpr@8..11
-          IdentExpr@8..11
+          NameRef@8..11
             Ident@8..11 "int"
       Comma@11..12 ","
       Emptyspace@12..13 " "
@@ -270,14 +263,14 @@ Root@0..37
         Colon@14..15 ":"
         Emptyspace@15..16 " "
         TypeExpr@16..19
-          IdentExpr@16..19
+          NameRef@16..19
             Ident@16..19 "int"
     RParen@19..20 ")"
     Emptyspace@20..21 " "
     Arrow@21..23 "->"
     Emptyspace@23..24 " "
     TypeExpr@24..28
-      IdentExpr@24..28
+      NameRef@24..28
         Ident@24..27 "int"
         Emptyspace@27..28 " "
     FunBody@28..37
@@ -285,12 +278,12 @@ Root@0..37
         LBrace@28..29 "{"
         Emptyspace@29..30 " "
         InfixExpr@30..36
-          IdentExpr@30..32
+          NameRef@30..32
             Ident@30..31 "a"
             Emptyspace@31..32 " "
           Plus@32..33 "+"
           Emptyspace@33..34 " "
-          IdentExpr@34..36
+          NameRef@34..36
             Ident@34..35 "b"
             Emptyspace@35..36 " "
         RBrace@36..37 "}""#]],
