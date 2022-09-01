cb4c869c4e66949f024862f370ac59f2c9a3d06b
David Mikulis <davidmikulis@gmail.com>
2022-09-01 13:28:12 -0500

    feat: add a basic hard-coded CLI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let input = fs::read_to_string("test.txt")?;
+    println!("{input}");
+
+    let parsed = parser::parse(&input);
+
+    println!("{}", &parsed.debug_tree());
+    println!();
+
+    Ok(())
 }
