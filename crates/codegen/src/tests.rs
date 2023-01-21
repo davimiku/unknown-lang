f7af720223709b1377e30d3ff094c5d9ded7925c
David Mikulis <davidmikulis@gmail.com>
2023-01-20 19:06:17 -0600

    refactor: remove unnecessary tests
    Now that we know reading bytes works, these tests aren't helpful in the long run

--- crates/codegen/src/tests.rs
+++ crates/codegen/src/tests.rs
@@ -14,42 +14,6 @@ fn test_print_call() {
 }
 
 #[test]
-fn test_read_byte() {
-    let mut chunk = Chunk::new();
-
-    let input = 255;
-
-    chunk.write_byte(input);
-    let actual = chunk.read::<u8>(0);
-
-    assert_eq!(input, actual);
-}
-
-#[test]
-fn test_read_int_constant() {
-    let mut chunk = Chunk::new();
-
-    let input = 8_000_000_000;
-
-    chunk.write_int_constant(input, 1);
-    let actual = chunk.read::<i64>(1);
-
-    assert_eq!(input, actual);
-}
-
-#[test]
-fn test_read_float_constant() {
-    let mut chunk = Chunk::new();
-
-    let input = 8_000_000_000.0;
-
-    chunk.write_float_constant(input, 1);
-    let actual = chunk.read::<f64>(1);
-
-    assert_eq!(input, actual);
-}
-
-#[test]
 fn test_print_input() {
     //
 }
