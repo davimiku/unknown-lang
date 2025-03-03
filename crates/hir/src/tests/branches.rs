932801c26b4b46cbaea4f4aa73c05dbf8ad34ffc
David <36610621+davimiku@users.noreply.github.com>
2025-03-02 22:54:49 -0600

    fix tests

--- crates/hir/src/tests/branches.rs
+++ crates/hir/src/tests/branches.rs
@@ -33,48 +33,52 @@ main~1.1 : ((red | green | blue)) -> Int = fun "main"(scrutinee~1.2 : (red | gre
 #[test]
 fn match_with_variant_payloads() {
     let input = "
-type Union = (a: Int | b: Int)
+type Union = (a: Int | b: Float)
 let main = fun (scrutinee: Union) -> Int {
     match scrutinee {
         .a a_int -> { 2 },
-        .b b_int -> { 3 },
+        .b b_float -> { 3 },
     }
 }";
     let expected = indoc! {r#"
-    Union~1.0 := a: Int~0.1 | b: Int~0.1
-main~1.1 : ((a: Int | b: Int)) -> Int = fun "main"(scrutinee~1.2 : (a: Int | b: Int)) -> Int { match scrutinee~1.2{
+    Union~1.0 := a: Int~0.1 | b: Float~0.2
+main~1.1 : ((a: Int | b: Float)) -> Int = fun "main"(scrutinee~1.2 : (a: Int | b: Float)) -> Int { match scrutinee~1.2{
     
     
 }; };"#};
 
     let expected_vars = &[
-        ("main~1.1", "((a: Int | b: Int)) -> Int"),
-        ("scrutinee~1.2", "(a: Int | b: Int)"),
+        ("main~1.1", "((a: Int | b: Float)) -> Int"),
+        ("scrutinee~1.2", "(a: Int | b: Float)"),
+        ("a_int~1.3", "Int"),
+        ("b_float~1.4", "Float"),
     ];
 
     check(input, expected, expected_vars);
 }
 
 #[test]
-fn match_with_inner_patterns() {
+fn match_with_variant_ident_binding() {
     let input = "
-type Union = (a: Int | b: Int)
+type Union = (a: Int | b: Float)
 let main = fun (scrutinee: Union) -> Int {
     match scrutinee {
         .a a_int -> { a_int },
-        .b b_int -> { b_int },
+        .b b_float -> { 0 },
     }
 }";
     let expected = indoc! {r#"
-    Union~1.0 := a: Int~0.1 | b: Int~0.1
-main~1.1 : ((a: Int | b: Int)) -> Int = fun "main"(scrutinee~1.2 : (a: Int | b: Int)) -> Int { match scrutinee~1.2{
+    Union~1.0 := a: Int~0.1 | b: Float~0.2
+main~1.1 : ((a: Int | b: Float)) -> Int = fun "main"(scrutinee~1.2 : (a: Int | b: Float)) -> Int { match scrutinee~1.2{
     
     
 }; };"#};
 
     let expected_vars = &[
-        ("main~1.1", "((a: Int | b: Int)) -> Int"),
-        ("scrutinee~1.2", "(a: Int | b: Int)"),
+        ("main~1.1", "((a: Int | b: Float)) -> Int"),
+        ("scrutinee~1.2", "(a: Int | b: Float)"),
+        ("a_int~1.3", "Int"),
+        ("b_float~1.4", "Float"),
     ];
 
     check(input, expected, expected_vars);
