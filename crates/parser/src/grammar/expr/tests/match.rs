e70483a8a02844c08d8bf5f652b1246e9881369f
David <36610621+davimiku@users.noreply.github.com>
2025-12-27 18:31:49 -0600

    Adjust match arm to be a block, which is what the MIR supports for now

--- crates/parser/src/grammar/expr/tests/match.rs
+++ crates/parser/src/grammar/expr/tests/match.rs
@@ -160,159 +160,172 @@ fn unwrap_add_and_rewrap() {
     let input = "type Number = (int: Int | float: Float)
 let main = fun (n: Number) -> {
     match n {
-        .int i -> Number.int (i + 16)
-        .float f -> Number.float (f + 16.0)
+        .int i -> { Number.int (i + 16) }
+        .float f -> { Number.float (f + 16.0) }
     }
 }";
-    check(input, expect![[r#"
-        Root@0..175
-          TypeBinding@0..39
-            TypeKw@0..4 "type"
-            Emptyspace@4..5 " "
-            Ident@5..12
-              Ident@5..11 "Number"
-              Emptyspace@11..12 " "
-            Equals@12..13 "="
-            Emptyspace@13..14 " "
-            TypeExpr@14..39
-              ParenExpr@14..39
-                LParen@14..15 "("
-                InfixExpr@15..38
-                  CompoundTypeItem@15..24
-                    Ident@15..18
-                      Ident@15..18 "int"
-                    Colon@18..19 ":"
-                    Emptyspace@19..20 " "
-                    CompoundTypeItemType@20..24
-                      Ident@20..24
-                        Ident@20..23 "Int"
-                        Emptyspace@23..24 " "
-                  Bar@24..25 "|"
-                  Emptyspace@25..26 " "
-                  CompoundTypeItem@26..38
-                    Ident@26..31
-                      Ident@26..31 "float"
-                    Colon@31..32 ":"
-                    Emptyspace@32..33 " "
-                    CompoundTypeItemType@33..38
-                      Ident@33..38
-                        Ident@33..38 "Float"
-                RParen@38..39 ")"
-          Newline@39..40
-            Newline@39..40 "\n"
-          LetBinding@40..175
-            LetKw@40..43 "let"
-            Emptyspace@43..44 " "
-            Ident@44..49
-              Ident@44..48 "main"
-              Emptyspace@48..49 " "
-            Equals@49..50 "="
-            Emptyspace@50..51 " "
-            FunExpr@51..175
-              FunKw@51..54 "fun"
-              Emptyspace@54..55 " "
-              FunParamList@55..67
-                LParen@55..56 "("
-                FunParam@56..65
-                  Ident@56..57
-                    Ident@56..57 "n"
-                  Colon@57..58 ":"
-                  Emptyspace@58..59 " "
-                  TypeExpr@59..65
-                    Ident@59..65
-                      Ident@59..65 "Number"
-                RParen@65..66 ")"
-                Emptyspace@66..67 " "
-              Arrow@67..69 "->"
-              Emptyspace@69..70 " "
-              FunBody@70..175
-                BlockExpr@70..175
-                  LBrace@70..71 "{"
-                  Newline@71..72 "\n"
-                  Emptyspace@72..76 "    "
-                  MatchExpr@76..173
-                    MatchKw@76..81 "match"
-                    Emptyspace@81..82 " "
-                    ScrutineeExpr@82..84
-                      PathExpr@82..84
-                        Ident@82..84
-                          Ident@82..83 "n"
-                          Emptyspace@83..84 " "
-                    MatchBlock@84..173
-                      LBrace@84..85 "{"
-                      Newline@85..86 "\n"
-                      Emptyspace@86..94 "        "
-                      MatchArm@94..132
-                        DotPattern@94..101
-                          Dot@94..95 "."
-                          Ident@95..99
-                            Ident@95..98 "int"
-                            Emptyspace@98..99 " "
-                          IdentPattern@99..101
-                            Ident@99..101
-                              Ident@99..100 "i"
-                              Emptyspace@100..101 " "
-                        Arrow@101..103 "->"
-                        Emptyspace@103..104 " "
-                        Call@104..123
-                          PathExpr@104..115
-                            Ident@104..110
-                              Ident@104..110 "Number"
-                            Dot@110..111 "."
-                            PathExpr@111..115
-                              Ident@111..115
-                                Ident@111..114 "int"
-                                Emptyspace@114..115 " "
-                          CallArgs@115..123
-                            LParen@115..116 "("
-                            InfixExpr@116..122
-                              PathExpr@116..118
-                                Ident@116..118
-                                  Ident@116..117 "i"
-                                  Emptyspace@117..118 " "
-                              Plus@118..119 "+"
-                              Emptyspace@119..120 " "
-                              IntLiteralExpr@120..122
-                                IntLiteral@120..122 "16"
-                            RParen@122..123 ")"
-                        Newline@123..124 "\n"
-                        Emptyspace@124..132 "        "
-                      MatchArm@132..172
-                        DotPattern@132..141
-                          Dot@132..133 "."
-                          Ident@133..139
-                            Ident@133..138 "float"
-                            Emptyspace@138..139 " "
-                          IdentPattern@139..141
-                            Ident@139..141
-                              Ident@139..140 "f"
-                              Emptyspace@140..141 " "
-                        Arrow@141..143 "->"
-                        Emptyspace@143..144 " "
-                        Call@144..167
-                          PathExpr@144..157
-                            Ident@144..150
-                              Ident@144..150 "Number"
-                            Dot@150..151 "."
-                            PathExpr@151..157
-                              Ident@151..157
-                                Ident@151..156 "float"
-                                Emptyspace@156..157 " "
-                          CallArgs@157..167
-                            LParen@157..158 "("
-                            InfixExpr@158..166
-                              PathExpr@158..160
-                                Ident@158..160
-                                  Ident@158..159 "f"
-                                  Emptyspace@159..160 " "
-                              Plus@160..161 "+"
-                              Emptyspace@161..162 " "
-                              FloatLiteralExpr@162..166
-                                FloatLiteral@162..166 "16.0"
-                            RParen@166..167 ")"
-                        Newline@167..168 "\n"
-                        Emptyspace@168..172 "    "
-                      RBrace@172..173 "}"
-                  Newline@173..174 "\n"
-                  RBrace@174..175 "}""#]]);
+    check(
+        input,
+        expect![[r#"
+            Root@0..183
+              TypeBinding@0..39
+                TypeKw@0..4 "type"
+                Emptyspace@4..5 " "
+                Ident@5..12
+                  Ident@5..11 "Number"
+                  Emptyspace@11..12 " "
+                Equals@12..13 "="
+                Emptyspace@13..14 " "
+                TypeExpr@14..39
+                  ParenExpr@14..39
+                    LParen@14..15 "("
+                    InfixExpr@15..38
+                      CompoundTypeItem@15..24
+                        Ident@15..18
+                          Ident@15..18 "int"
+                        Colon@18..19 ":"
+                        Emptyspace@19..20 " "
+                        CompoundTypeItemType@20..24
+                          Ident@20..24
+                            Ident@20..23 "Int"
+                            Emptyspace@23..24 " "
+                      Bar@24..25 "|"
+                      Emptyspace@25..26 " "
+                      CompoundTypeItem@26..38
+                        Ident@26..31
+                          Ident@26..31 "float"
+                        Colon@31..32 ":"
+                        Emptyspace@32..33 " "
+                        CompoundTypeItemType@33..38
+                          Ident@33..38
+                            Ident@33..38 "Float"
+                    RParen@38..39 ")"
+              Newline@39..40
+                Newline@39..40 "\n"
+              LetBinding@40..183
+                LetKw@40..43 "let"
+                Emptyspace@43..44 " "
+                Ident@44..49
+                  Ident@44..48 "main"
+                  Emptyspace@48..49 " "
+                Equals@49..50 "="
+                Emptyspace@50..51 " "
+                FunExpr@51..183
+                  FunKw@51..54 "fun"
+                  Emptyspace@54..55 " "
+                  FunParamList@55..67
+                    LParen@55..56 "("
+                    FunParam@56..65
+                      Ident@56..57
+                        Ident@56..57 "n"
+                      Colon@57..58 ":"
+                      Emptyspace@58..59 " "
+                      TypeExpr@59..65
+                        Ident@59..65
+                          Ident@59..65 "Number"
+                    RParen@65..66 ")"
+                    Emptyspace@66..67 " "
+                  Arrow@67..69 "->"
+                  Emptyspace@69..70 " "
+                  FunBody@70..183
+                    BlockExpr@70..183
+                      LBrace@70..71 "{"
+                      Newline@71..72 "\n"
+                      Emptyspace@72..76 "    "
+                      MatchExpr@76..181
+                        MatchKw@76..81 "match"
+                        Emptyspace@81..82 " "
+                        ScrutineeExpr@82..84
+                          PathExpr@82..84
+                            Ident@82..84
+                              Ident@82..83 "n"
+                              Emptyspace@83..84 " "
+                        MatchBlock@84..181
+                          LBrace@84..85 "{"
+                          Newline@85..86 "\n"
+                          Emptyspace@86..94 "        "
+                          MatchArm@94..136
+                            DotPattern@94..101
+                              Dot@94..95 "."
+                              Ident@95..99
+                                Ident@95..98 "int"
+                                Emptyspace@98..99 " "
+                              IdentPattern@99..101
+                                Ident@99..101
+                                  Ident@99..100 "i"
+                                  Emptyspace@100..101 " "
+                            Arrow@101..103 "->"
+                            Emptyspace@103..104 " "
+                            BlockExpr@104..127
+                              LBrace@104..105 "{"
+                              Emptyspace@105..106 " "
+                              Call@106..126
+                                PathExpr@106..117
+                                  Ident@106..112
+                                    Ident@106..112 "Number"
+                                  Dot@112..113 "."
+                                  PathExpr@113..117
+                                    Ident@113..117
+                                      Ident@113..116 "int"
+                                      Emptyspace@116..117 " "
+                                CallArgs@117..126
+                                  LParen@117..118 "("
+                                  InfixExpr@118..124
+                                    PathExpr@118..120
+                                      Ident@118..120
+                                        Ident@118..119 "i"
+                                        Emptyspace@119..120 " "
+                                    Plus@120..121 "+"
+                                    Emptyspace@121..122 " "
+                                    IntLiteralExpr@122..124
+                                      IntLiteral@122..124 "16"
+                                  RParen@124..125 ")"
+                                  Emptyspace@125..126 " "
+                              RBrace@126..127 "}"
+                            Newline@127..128 "\n"
+                            Emptyspace@128..136 "        "
+                          MatchArm@136..180
+                            DotPattern@136..145
+                              Dot@136..137 "."
+                              Ident@137..143
+                                Ident@137..142 "float"
+                                Emptyspace@142..143 " "
+                              IdentPattern@143..145
+                                Ident@143..145
+                                  Ident@143..144 "f"
+                                  Emptyspace@144..145 " "
+                            Arrow@145..147 "->"
+                            Emptyspace@147..148 " "
+                            BlockExpr@148..175
+                              LBrace@148..149 "{"
+                              Emptyspace@149..150 " "
+                              Call@150..174
+                                PathExpr@150..163
+                                  Ident@150..156
+                                    Ident@150..156 "Number"
+                                  Dot@156..157 "."
+                                  PathExpr@157..163
+                                    Ident@157..163
+                                      Ident@157..162 "float"
+                                      Emptyspace@162..163 " "
+                                CallArgs@163..174
+                                  LParen@163..164 "("
+                                  InfixExpr@164..172
+                                    PathExpr@164..166
+                                      Ident@164..166
+                                        Ident@164..165 "f"
+                                        Emptyspace@165..166 " "
+                                    Plus@166..167 "+"
+                                    Emptyspace@167..168 " "
+                                    FloatLiteralExpr@168..172
+                                      FloatLiteral@168..172 "16.0"
+                                  RParen@172..173 ")"
+                                  Emptyspace@173..174 " "
+                              RBrace@174..175 "}"
+                            Newline@175..176 "\n"
+                            Emptyspace@176..180 "    "
+                          RBrace@180..181 "}"
+                      Newline@181..182 "\n"
+                      RBrace@182..183 "}""#]],
+    );
 }
